884ced45d1842dd3c4adef34f2dd23a43e943738
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-01-11 07:27:08 -0600

    add type to database table and model

--- sql/views/View_Lists.sql
+++ sql/views/View_Lists.sql
@@ -2,7 +2,9 @@ CREATE VIEW View_Lists AS
 SELECT 
     `l`.`id` AS `id`,
     `l`.`name` AS `name`,
+    `l`.`type` AS `type`,
     `l`.`created_on` AS `created_on`,
+    List_Modified_On(`l`.`id`) as `modified_on`,
     COUNT(i.id) AS count_items
 FROM `Lists` `l`
 LEFT JOIN Items i ON i.list_id = l.id
--- src/api/api_lists/models/list.py
+++ src/api/api_lists/models/list.py
@@ -4,13 +4,21 @@ List model
 **********************************************************************************
 """
 
+from enum import Enum
 from dataclasses import dataclass
 from uuid import UUID
 from datetime import datetime
 
+
+class ListType(str, Enum):
+    LIST    : str = 'list'
+    TEMPLATE: str = 'template'
+
+
 @dataclass
 class List:
-    id        : UUID     = None
-    user_id   : UUID     = None
-    name      : str      = None
+    id         : UUID    = None
+    user_id    : UUID    = None
+    name       : str     = None
     created_on: datetime = None
+    type      : ListType = ListType.LIST
