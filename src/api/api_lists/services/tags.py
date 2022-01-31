117e8949d0df28a851605af6a46cf3e7782edf96
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-01-31 08:29:15 -0600

    api: get all the tags from the database

--- src/api/api_lists/services/tags.py
+++ src/api/api_lists/services/tags.py
@@ -6,7 +6,34 @@ This module contains all services related to tags.
 
 
 import flask
+from ..db_manager import commands as sql_engine, DbOperationResult
+from ..common import responses
 
+SQL_SELECT_ALL = '''
+    SELECT * FROM View_Tags vt
+    WHERE EXISTS (
+        SELECT 1 FROM Tags t
+        WHERE t.user_id = %s 
+        AND t.id = vt.id
+    )
+'''
 
+#------------------------------------------------------
+# Get all tags response
+#------------------------------------------------------
 def getAllTags() -> flask.Response:
-    return 'get all tags service'
+    sql_result = cmdSelectAll()
+
+    if not sql_result.successful:
+        return responses.badRequest(sql_result.error)
+    
+    return responses.get(sql_result.data)
+
+#------------------------------------------------------
+# Get all the user's tags from the database
+#------------------------------------------------------
+def cmdSelectAll() -> DbOperationResult:
+    sql = SQL_SELECT_ALL
+    parms = (str(flask.g.client_id), )
+
+    return sql_engine.select(sql, parms, True)
