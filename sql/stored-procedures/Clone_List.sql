28cbe0c2cd0d21f6b0e91ddf08a07c92ed4b86b7
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-02-07 20:24:07 -0600

    Update Clone_List.sql

--- sql/stored-procedures/Clone_List.sql
+++ sql/stored-procedures/Clone_List.sql
@@ -7,6 +7,7 @@ BEGIN
     -- This procedure creates a clone of the given list
     -- It creates a new list record with an id of the given new_list_id
     -- Then, it copies all the items belonging to the existing list into the new list
+    -- Then, it assigns all the tags that are currently assigned to the template
 
     -- create a new list record that has the given clone_id and name of the template list
     INSERT INTO Lists (id, user_id, name, `type`)
@@ -18,6 +19,12 @@ BEGIN
     SELECT UUID(), new_list_id, i.content, i.`rank`, i.complete
     FROM Items i
     WHERE i.list_id = template_id;
+    
+    -- copy over all the template's assigned tag records for the new list
+    INSERT INTO List_Tags (list_id, tag_id)
+	SELECT new_list_id, lt.tag_id
+	FROM List_Tags lt
+	WHERE lt.list_id = template_id;
     
 END$$
 DELIMITER ;
