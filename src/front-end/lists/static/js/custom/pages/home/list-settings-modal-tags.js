55e8afdeef091f512eb5a5e9840e4c3f91a378d8
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-02-07 20:19:14 -0600

    gui: mark assigned checkboxes to checked

--- src/front-end/lists/static/js/custom/pages/home/list-settings-modal-tags.js
+++ src/front-end/lists/static/js/custom/pages/home/list-settings-modal-tags.js
@@ -18,23 +18,31 @@ export class ListSettingsModalTags
         // copy over the badges from the active list
         const eTags = ListHtml.getAssignedTags(eActiveListContainer).clone();
         $(ListSettingsModalTags.Elements.ASSIGNED).html(eTags);
-
-        // Get a list of the current list's assigned tag ids
-        const assignedTagIDs = ListSettingsModalTags.getAssignedTagIDs();
-        console.log(assignedTagIDs);
-    
-
+            
         // uncheck all the list boxes to start
         const eItemCheckboxes = ListSettingsModalTags.getAllItemCheckboxElements();
         ListSettingsModalTags.setCheckboxesTo(eItemCheckboxes, false);
+
+        // Get a list of the current list's assigned tag ids
+        const assignedTagIDs = ListSettingsModalTags.getAssignedTagIDs(eTags);
+
+        // get a filtered down collection of checkbox elements - ones that have values found in the assigned tag id list
+        const eAssignedCheckboxes = $(eItemCheckboxes).filter(function() {
+            const checkboxValue = $(this).val();
+            return assignedTagIDs.includes(checkboxValue);
+        });
+
+        // mark the assigned tag elements as checked
+        ListSettingsModalTags.setCheckboxesTo(eAssignedCheckboxes, true);
     }
 
     
     /**********************************************************
     Get a list of the current list's assigned tag ids
     **********************************************************/
-    static getAssignedTagIDs() {
+    static getAssignedTagIDs(eTags) {
         const assignedTagIDs = [];
+
         $(eTags).each(function() {
             const tagID = $(this).attr('data-tag-id');
             assignedTagIDs.push(tagID);
--- src/front-end/lists/static/js/custom/rollup.config.js
+++ src/front-end/lists/static/js/custom/rollup.config.js
@@ -6,6 +6,7 @@ export default [
             file: 'dist/home.bundle.js',
             format: 'iife',
             compact: true,
+            sourcemap: true,
         }
     },
     {
@@ -14,6 +15,7 @@ export default [
             file: 'dist/login.bundle.js',
             format: 'iife',
             compact: true,
+            sourcemap: true,
         }
     },
     {
@@ -22,6 +24,7 @@ export default [
             file: 'dist/tags.bundle.js',
             format: 'iife',
             compact: true,
+            sourcemap: true,
         }
     },
   ];
