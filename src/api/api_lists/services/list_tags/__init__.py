a0d0aa29f03d9a2c5b131418ae88faa110f5f438
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-02-23 07:43:08 -0600

    add all the new modules to the init files

--- src/api/api_lists/services/list_tags/__init__.py
+++ src/api/api_lists/services/list_tags/__init__.py
@@ -1,1 +1,4 @@
 from .routines import *
+
+from . import sql_commands as sql_commands
+from . import sql_stmts as sql_stmts
--- src/api/api_lists/services/tags/__init__.py
+++ src/api/api_lists/services/tags/__init__.py
@@ -1,3 +1,4 @@
 from .routines import *
 
 from . import sql_statements as sql_statements 
+from . import sql_commands as sql_commands
